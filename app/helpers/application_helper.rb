95374d03feb3b352eb1e00025dbe17c2cb5561ef
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-01-09 13:10:45 +0900

    Bootstrapの導入とCSSなど見た目の調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
-end
+    def bootstrap_class_for(flash_type)
+      case flash_type.to_sym
+      when :notice
+        "alert-success" # 緑色
+      when :alert
+        "alert-danger"  # 赤色
+      when :error
+        "alert-warning" # 黄色
+      else
+        "alert-info"    # 青色
+      end
+    end
+  end
